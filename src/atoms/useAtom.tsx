67da85b0e50e2b4a1067a2858d8c54b2b583fc05
Henrik Collin <henrik.collin@cinia.fi>
2020-05-26 14:53:39 +0300

    v0.4.3

--- src/atoms/useAtom.tsx
+++ src/atoms/useAtom.tsx
@@ -12,24 +12,40 @@ export default function useAtom<T>(
 
     useEffect(() => {
         if (!joki.atom.has(atomId)) {
+            console.log(
+                `jokits-react: useAtom: creating new atom ${atomId} with value ${defaultValue}`
+            );
             joki.atom.set(atomId, defaultValue);
         }
 
         const a: JokiAtom<T> = joki.atom.get<T>(atomId);
+        const aVal: T = a.get();
+        console.log(
+            `jokits-react: useAtom: Atom ${atomId} currently has value ${aVal}`
+        );
         setAtom(a);
-        setValue(a.get());
+        setValue(aVal);
 
         const stop = a.subscribe((val: T) => {
+            console.log(
+                `jokits-react: useAtom: ${atomId} value updated to ${val}`
+            );
             setValue(val);
         });
 
         return () => {
+            console.log(
+                `jokits-react: useAtom: ${atomId} stop listening changes`
+            );
             stop();
         };
     }, [atomId, defaultValue]);
 
     const updateAtomValue = useCallback(
         (val: T): void => {
+            console.log(
+                `jokits-react: useAtom: ${atomId} update atom value to ${val} by callback`
+            );
             if (atom !== null) {
                 atom.set(val);
             }
--- src/atoms/useAtomValue.tsx
+++ src/atoms/useAtomValue.tsx
@@ -8,17 +8,30 @@ export default function useAtomValue<T>(atomId: string, defaultValue: T) {
 
     useEffect(() => {
         if (!joki.atom.has(atomId)) {
+            console.log(
+                `jokits-react: useAtomValue: creating new atom ${atomId} with value ${defaultValue}`
+            );
             joki.atom.set(atomId, defaultValue);
         }
 
         const a: JokiAtom<T> = joki.atom.get<T>(atomId);
-        setValue(a.get());
+        const aVal: T = a.get();
+        console.log(
+            `jokits-react: useAtomValue: Atom ${atomId} currently has value ${aVal}`
+        );
+        setValue(aVal);
 
         const stop = a.subscribe((val: T) => {
+            console.log(
+                `jokits-react: useAtomValue: ${atomId} value updated to ${val}`
+            );
             setValue(val);
         });
 
         return () => {
+            console.log(
+                `jokits-react: useAtomValue: ${atomId} stop listening changes`
+            );
             stop();
         };
     }, [atomId, defaultValue]);
