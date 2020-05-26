f2d7c366193abf06909a40c00f270eb7a17541b3
Henrik Collin <henrik.collin@cinia.fi>
2020-05-26 10:56:50 +0300

    Added eventIs and process Tools

---
+++ src/tools/process.ts
@@ -0,0 +1,90 @@
+import { JokiEvent } from 'jokits';
+import joki from '../joki';
+
+const existingProcesses: Set<string> = new Set<string>();
+
+export interface ProcessCycleActions {
+    begin: (nameOrId: string) => void;
+    done: (nameOrId: string) => void;
+}
+
+export function createProcess(processName: string, targetCount?: number) {
+    if (existingProcesses.has(processName))
+        throw new Error(`Process ${processName} exists already!`);
+
+    existingProcesses.add(processName);
+
+    const processingUnitsRunning: Set<string> = new Set<string>();
+    const processingUnitsDone: Set<string> = new Set<string>();
+
+    let state: 'INIT' | 'INPROGRESS' | 'DONE' = 'INIT';
+
+    function startProcess(): Promise<string[]> {
+        state = 'INPROGRESS';
+        return new Promise((resolve) => {
+            const cycle: ProcessCycleActions = {
+                begin: (nameOrId: string) => {
+                    if (state === 'DONE') {
+                        throw new Error(
+                            `Process ${processName} was already finished when ${nameOrId} tried to start its cycle.`
+                        );
+                    }
+                    processingUnitsRunning.add(nameOrId);
+                },
+                done: (nameOrId: string) => {
+                    if (state === 'DONE') {
+                        throw new Error(
+                            `Process ${processName} was already finished when ${nameOrId} tried to end it's cycle.`
+                        );
+                    }
+                    if (processingUnitsRunning.has(nameOrId)) {
+                        processingUnitsDone.add(nameOrId);
+                        if (
+                            processingUnitsDone.size ===
+                                processingUnitsRunning.size ||
+                            processingUnitsDone.size === targetCount
+                        ) {
+                            state = 'DONE';
+                            resolve(Array.from(processingUnitsDone));
+                        }
+                    } else {
+                        throw new Error(
+                            `Process unit ${nameOrId} tried to be done before it even started.`
+                        );
+                    }
+                },
+            };
+
+            const procesStartEvent: JokiEvent = {
+                from: processName,
+                action: 'startProcess',
+                data: cycle,
+            };
+
+            joki.trigger(procesStartEvent);
+        });
+    }
+
+    function endProcess(forceEnd = false) {
+        if (state !== 'DONE' && forceEnd !== true) {
+            throw new Error(`Process is not finished yet!`);
+        }
+        existingProcesses.delete(processName);
+    }
+
+    return {
+        startProcess,
+        endProcess,
+    };
+}
+
+export function isProcessEvent(
+    event: JokiEvent
+): undefined | [string, ProcessCycleActions] {
+    if (event.from && existingProcesses.has(event.from)) {
+        if (event.action === 'startProcess' && event.data) {
+            return [event.from, event.data as ProcessCycleActions];
+        }
+    }
+    return undefined;
+}
